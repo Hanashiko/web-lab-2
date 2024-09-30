592036447f00e78a27f3da44e6a0b6b49d68a904
Margin-enterteiment <119576487+Margin-interteiment@users.noreply.github.com>
2024-10-01 00:58:12 +0300

    Fixed problems

---
+++ README.md
@@ -0,0 +1,8 @@
+# Макет сайту      
+
+### Комп'ютерна версія:
+![PC_Screenshot](./docs/web-lab2-pc.png)
+
+### Телефона версія:
+![Phone_Screenshot_1](./docs/web-lab2-phone1.png)
+![Phone_Screenshot_2](./docs/web-lab2-phone2.png)
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pub/*.{html, css}"],
+  theme: {
+    extend: {    
+      fontFamily: {
+        sofadi: ['"Sofadi One"', "system-ui"],
+        oswald: ['"Oswald"', "sans-serif"],
+        rubik: ['"Rubik"', "sans-serif"],
+        montserrat: ['"Montserrat"', 'sans-serif'],
+    },
+  },
+  screens: {
+    'esm': '300px',
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+    '2xl': '1536px',
+  },
+
+  },
+  plugins: [],
+}
+
