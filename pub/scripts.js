554ae5e0ab2a5d662123aef7b36a035089688c0d
Hanashiko <hlichisper@gmail.com>
2024-10-01 00:12:05 +0300

    Edit responsive design

---
+++ pub/scripts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log("info")
+
+let js = [];
+
+console.log(typeof '3123')
+
+if (typeof '3123' === Object && js.every) {
+    console.log("ok 3123")
+}
+
+if (typeof js === Object && js.every) {
+    console.log("ok")
+}
